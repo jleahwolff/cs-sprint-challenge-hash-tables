e9dde5d01022e7d0d16a0f67e1a92fa9c15eac43
Jessica Wolff <jleahwolff@gmail.com>
2020-07-10 11:08:08 -0500

    Done with ex1

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,24 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+#a is a list of numbers both positive and negative
+#assuming there are no repeated numbers(like in test and ex.)
+#make each number in list be positive using abs()
+#count them in cache
+#those who have a count of 2 append to result list
+
+def has_negatives(a):
+    count={}
+    result=[]
+    a = [abs(i) for i in a]
+    
+    for i in a:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+
+    for key in count:
+        if count[key] == 2:
+            result.append(key)
     return result
 
 
